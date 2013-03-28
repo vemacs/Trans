d88e11af2942435cc30335a363254dba35a805d4
vemacs <gentoo@outlook.com>
2013-03-27 18:35:45 -0600

    URL handling

--- src/main/java/com/nullblock/vemacs/trans/Trans.java
+++ src/main/java/com/nullblock/vemacs/trans/Trans.java
@@ -1,6 +1,13 @@
 package com.nullblock.vemacs.trans;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -25,14 +32,30 @@ public class Trans extends JavaPlugin implements Listener {
     	Boolean uppercase = this.getConfig().getBoolean("uppercase");
     	Boolean bigben = false;
         String message = event.getMessage();
-        
+        HashMap hm = new HashMap(); 
+        Pattern p = Pattern
+				.compile("(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?������]))");
+		Matcher m = p.matcher(message);
+		StringBuffer sb = new StringBuffer();
+		String urlTmp = "";
+		while (m.find()) {
+			urlTmp = m.group(1);
+			String uuid = UUID.randomUUID().toString().replace("-", "");
+			hm.put(uuid, urlTmp);
+            message.replace(urlTmp, uuid);
+			m.appendReplacement(sb, "");
+			sb.append(urlTmp);
+		}
+		m.appendTail(sb);
+		message = sb.toString();
         message = TransUtil.getTranslation(message, lang);
-        message.replace("nhttp", "http");
-        message.replace("/ ", "/");
-        message.replace(" /", "/");
-        message.replace("- ", "-");
-        message.replace(" -", "-");
-        message.replace(" :", ":");
+        Set set = hm.entrySet(); 
+        Iterator i = set.iterator(); 
+        while(i.hasNext()) { 
+        	Map.Entry me = (Map.Entry)i.next(); 
+        	message.replace(me.getKey().toString(), me.getValue().toString()); 
+        	} 
+
         if( uppercase == true ){
         message = message.toUpperCase(new Locale(lang));
         }
