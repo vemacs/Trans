403b9f58be882f96c5c999478761f2a623cbe2a1
vemacs <gentoo@outlook.com>
2013-10-26 11:41:32 -0600

    minor refactor

--- src/main/java/com/nullblock/vemacs/trans/Trans.java
+++ src/main/java/com/nullblock/vemacs/trans/Trans.java
@@ -1,7 +1,5 @@
 package com.nullblock.vemacs.trans;
 
-import java.util.Locale;
-
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -11,42 +9,46 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Locale;
+
 
 public class Trans extends JavaPlugin implements Listener {
-	public void onDisable() {
-	}
 
-	public void onEnable() {
-		getServer().getPluginManager().registerEvents(this, this);
-		this.saveDefaultConfig();
-	}
+    public void onDisable() {
+    }
 
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerChat(AsyncPlayerChatEvent event) {
-		String lang = this.getConfig().getString("lang");
-		Boolean uppercase = this.getConfig().getBoolean("uppercase");
-		Boolean bigben = false;
-		String message = event.getMessage();
-		if(message.startsWith("bigben: ")){
-			bigben = true;
-			message = message.substring(8);
-		}
-		message = TransUtil.getTranslation(message, lang);
-		if( bigben == true){
-			message = "bigben: " + message;
-		}
-		if( uppercase == true ){
-			message = message.toUpperCase(new Locale(lang));
-		}
-		event.setMessage(message);
-	}
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        this.saveDefaultConfig();
+    }
 
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		if(cmd.getName().equalsIgnoreCase("trans") && sender.hasPermission("trans.reload")){
-			this.reloadConfig();
-			sender.sendMessage(ChatColor.GREEN + "Trans has been reloaded.");
-			return true;
-		} 
-		return false; 
-	}
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        String lang = this.getConfig().getString("lang");
+        Boolean uppercase = this.getConfig().getBoolean("uppercase");
+        Boolean bigben = false;
+        String message = event.getMessage();
+        if (message.startsWith("bigben: ")) {
+            bigben = true;
+            message = message.substring(8);
+        }
+        message = TransUtil.getTranslation(message, lang);
+        if (bigben) {
+            message = "bigben: " + message;
+        }
+        if (uppercase) {
+            message = message.toUpperCase(new Locale(lang));
+        }
+        event.setMessage(message);
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("trans") && sender.hasPermission("trans.reload")) {
+            this.reloadConfig();
+            sender.sendMessage(ChatColor.GREEN + "Trans has been reloaded.");
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/main/java/com/nullblock/vemacs/trans/TransUtil.java
+++ src/main/java/com/nullblock/vemacs/trans/TransUtil.java
@@ -1,125 +1,119 @@
 package com.nullblock.vemacs.trans;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.apache.commons.lang.StringUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TransUtil {
 
-	public static String readURL(String url) {
-		String response = "";
-		try {
-			URL toread = new URL(url);
-			URLConnection yc = toread.openConnection();
-			// Yahoo uses this UserAgent, so might as well use it to prevent 403s
-			yc.setRequestProperty("User-Agent", "Mozilla/5.0");
-			BufferedReader in = new BufferedReader(new InputStreamReader(yc
-					.getInputStream(), "UTF-8"));
-			String inputLine;
-			while ((inputLine = in.readLine()) != null) {
-				response = response + inputLine;
-			}
-			in.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return response;
-	}
+    public static String readURL(String url) {
+        String response = "";
+        try {
+            URL toRead = new URL(url);
+            URLConnection yc = toRead.openConnection();
+            // Yahoo uses this UserAgent, so might as well use it to prevent 403s
+            yc.setRequestProperty("User-Agent", "Mozilla/5.0");
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc
+                    .getInputStream(), "UTF-8"));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response = response + inputLine;
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
 
-	public static String getTranslation(String text, String lang){
-		HashMap hm = new HashMap(); 
-		Pattern p = Pattern
-				.compile("(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½]))");
-		Matcher m = p.matcher(text);
-		StringBuffer sb = new StringBuffer();
-		String urlTmp = "";
-		// URL handling
-		while (m.find()) {
-			urlTmp = m.group(1);
-			String uuid = UUID.randomUUID().toString().replace("-", "");
-			hm.put(uuid, urlTmp);
-			text.replace(urlTmp, uuid);
-			m.appendReplacement(sb, "");
-			sb.append(urlTmp);
-		}
-		m.appendTail(sb);
-		text = sb.toString();
-		// end replace with UUID
-		text = URLEncoder.encode(text);
-		String response = readURL("http://translate.google.com/translate_a/t?q=" + text + "&client=p&text=&sl=auto&tl=" + lang + "&ie=UTF-8&oe=UTF-8");
-		response = parse(response);
+    public static String getTranslation(String text, String lang) {
+        HashMap<String, String> hm = new HashMap<String, String>();
+        Pattern p = Pattern
+                .compile("(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½]))");
+        Matcher m = p.matcher(text);
+        StringBuffer sb = new StringBuffer();
+        String urlTmp = "";
+        // URL handling
+        while (m.find()) {
+            urlTmp = m.group(1);
+            String uuid = UUID.randomUUID().toString().replace("-", "");
+            hm.put(uuid, urlTmp);
+            text = text.replace(urlTmp, uuid);
+            m.appendReplacement(sb, "");
+            sb.append(urlTmp);
+        }
+        m.appendTail(sb);
+        text = sb.toString();
+        // end replace with UUID
+        text = URLEncoder.encode(text);
+        String response = readURL("http://translate.google.com/translate_a/t?q=" + text + "&client=p&text=&sl=auto&tl=" + lang + "&ie=UTF-8&oe=UTF-8");
+        response = parse(response);
 
-		// begin UUID to URL		
-		Set set = hm.entrySet(); 
-		Iterator i = set.iterator(); 
-		while(i.hasNext()) { 
-			Map.Entry me = (Map.Entry)i.next(); 
-			response.replace(me.getKey().toString(), me.getValue().toString()); 
-		} 
-		// end UUID to URL
-		response = postProcess(response, lang);
-		return response;
-	}
+        // begin UUID to URL
+        Set<Map.Entry<String,String>> set = hm.entrySet();
+        for (Map.Entry<String, String> me : set) {
+            response = response.replace(me.getKey(), me.getValue());
+        }
+        // end UUID to URL
+        response = postProcess(response, lang);
+        return response;
+    }
 
-	public static String postProcess(String response, String lang){
-		// post processing text
-		response = response.replace(" :", ":");
-		response = response.replace(" ,", ",");
-		response = response.replace(". / ", "./");
+    public static String postProcess(String response, String lang) {
+        // post processing text
+        response = response.replace(" :", ":");
+        response = response.replace(" ,", ",");
+        response = response.replace(". / ", "./");
 
-		if( response.startsWith("¿") && StringUtils.countMatches(response, "?") == 0){
-			response = response + "?";
-		}
-		if( response.startsWith("¡") && StringUtils.countMatches(response, "!") == 0 ){
-			response = response + "!";
-		}
-		if( lang.equals("en") && response.startsWith("'re")){
-			response = "You" + response;
-		}
-		return response;
-	}
-	
-	public static String parse(String response) {
-		JSONParser parser = new JSONParser();
-		JSONObject obj = new JSONObject();
-		try {
-			obj = (JSONObject) parser.parse(response);
-		} catch (ParseException e) {
-		}
-		JSONArray sentences = (JSONArray) obj.get("sentences");
-		String finalresponse = "";
-		for(int i = 0; i < sentences.size(); i++ ){
-			String line = "" + sentences.get(i);
-			String trans = getTrans(line);
-			finalresponse = finalresponse + trans;
-		}
-		return finalresponse;
-	}
-	
-	public static String getTrans (String sentence)
-	{
-		JSONParser parser = new JSONParser();
-		JSONObject obj = new JSONObject();
-		try {
-			obj = (JSONObject) parser.parse(sentence);
-		} catch (ParseException e) {
-		}
-		sentence = (String) obj.get("trans");
-		return sentence;
-	}
+        if (response.startsWith("\u00BF") && StringUtils.countMatches(response, "?") == 0) {
+            response = response + "?";
+        }
+        if (response.startsWith("\u00A1") && StringUtils.countMatches(response, "!") == 0) {
+            response = response + "!";
+        }
+        if (lang.equals("en") && response.startsWith("'re")) {
+            response = "You" + response;
+        }
+        return response;
+    }
+
+    public static String parse(String response) {
+        JSONParser parser = new JSONParser();
+        JSONObject obj = new JSONObject();
+        try {
+            obj = (JSONObject) parser.parse(response);
+        } catch (ParseException ignored) {
+        }
+        JSONArray sentences = (JSONArray) obj.get("sentences");
+        String finalResponse = "";
+        for (Object sentence : sentences) {
+            String line = "" + sentence;
+            String trans = getTrans(line);
+            finalResponse = finalResponse + trans;
+        }
+        return finalResponse;
+    }
+
+    public static String getTrans(String sentence) {
+        JSONParser parser = new JSONParser();
+        JSONObject obj = new JSONObject();
+        try {
+            obj = (JSONObject) parser.parse(sentence);
+        } catch (ParseException ignored) {
+        }
+        sentence = (String) obj.get("trans");
+        return sentence;
+    }
+
 }
